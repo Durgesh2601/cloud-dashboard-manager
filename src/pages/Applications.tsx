441b3f59cd0f4c24b7196ccc467298d5285b8398
Durgesh2601 <dk829445@gmail.com>
2024-05-03 21:58:02 +0530

    feat: add transition in side bar and content

--- src/pages/Applications.tsx
+++ src/pages/Applications.tsx
@@ -1,29 +1,45 @@
+import { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import TopBar from "../components/Applications/TopBar";
 import AppsHeader from "../components/Applications/Header";
 import AppTabs from "../components/Applications/Tabs";
 import { useLayout } from "../context/LayoutContext";
 
+const TransitionGrid = styled(Grid)<{ drawerWidth: number }>(
+  ({ theme, drawerWidth }) => ({
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: drawerWidth,
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+    },
+  })
+);
+
 const Applications = () => {
   const { isDrawerOpen } = useLayout();
+  const [drawerWidth, setDrawerWidth] = useState(0);
+
+  useEffect(() => {
+    setDrawerWidth(isDrawerOpen ? 200 : 65);
+  }, [isDrawerOpen]);
+
   return (
-    <Grid
-      item
-      xs
-      position="absolute"
-      right={0}
-      left={isDrawerOpen ? 200 : 60}
-      p={1}
-    >
-      <TopBar />
-      <AppsHeader
-        application={{
-          id: 1,
-          name: "tic-tac-toc",
-        }}
-      />
-      <AppTabs />
-    </Grid>
+    <TransitionGrid drawerWidth={drawerWidth}>
+      <Grid item xs>
+        <TopBar />
+        <AppsHeader
+          application={{
+            id: 1,
+            name: "tic-tac-toc",
+          }}
+        />
+        <AppTabs />
+      </Grid>
+    </TransitionGrid>
   );
 };
 
