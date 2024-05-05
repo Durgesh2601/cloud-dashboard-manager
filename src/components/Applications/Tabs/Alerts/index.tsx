36100067084ddeeda6c0ded1840d7215bc9c5da1
Durgesh2601 <dk829445@gmail.com>
2024-05-05 13:25:48 +0530

    feat: alerts empty screen

--- src/components/Applications/Tabs/Alerts/index.tsx
+++ src/components/Applications/Tabs/Alerts/index.tsx
@@ -1,5 +1,18 @@
+import { Grid, Typography } from "@mui/material";
+import { commonGridStyles } from "../../../../constants";
+
 const Alerts = () => {
-  return <>Alerts</>;
+  return (
+    <Grid
+      item
+      sx={{
+        ...commonGridStyles,
+      }}
+      p={3}
+    >
+      <Typography variant="h5">No Alerts</Typography>
+    </Grid>
+  );
 };
 
 export default Alerts;
